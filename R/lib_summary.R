fbaeb08d3852ee6e67d6ac33d8ee334bc55fc2df
Russ Conte <russconte@mac.com>
2023-09-18 12:27:04 -0500

    fixed coding error.

--- R/lib_summary.R
+++ R/lib_summary.R
@@ -6,10 +6,11 @@
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function(){
-  pkgs <- utils::install.packages()
+lib_summary <- function() {
+  pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
-  names(pkg_df) <- c("library")
+  names(pkg_df) <- c("Library", "n_packages")
+
   pkg_df
 }
