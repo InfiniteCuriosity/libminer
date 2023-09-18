4227b615d0d67aa40ba9a7cef0006af0850ed681
Russ Conte <russconte@mac.com>
2023-09-18 11:03:59 -0500

    Add lib summary function

---
+++ R/lib_summary.R
@@ -0,0 +1,7 @@
+lib_summary <- function(){
+  pkgs <- utils::install.packages()
+  pkg_tbl <- table(pkgs[, "LibPath"])
+  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
+  names(pkg_df) <- c("library")
+  pkg_df
+}
