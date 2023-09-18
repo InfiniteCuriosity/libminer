80c28e0e5be65bd10e5b4a37878ef4dd537d26b7
Russ Conte <russconte@mac.com>
2023-09-18 15:43:49 -0500

    Added ReadMe and ReadMe.RMD

--- R/lib_summary.R
+++ R/lib_summary.R
@@ -6,7 +6,10 @@
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+lib_summary <- function(sizes = FALSE) {
+  if(!is.logical(sizes)){
+    stop("Sizes must be a logial function (TRUE/FALSE)")
+  }
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
--- tests/testthat/test-lib_summary.R
+++ tests/testthat/test-lib_summary.R
@@ -6,3 +6,32 @@ test_that("Lib summary returns expected results", {
   expect_type(res3(library, "character"))
   expect_type(res$n_packages, "interger")
 })
+
+if (isTRUE(sizes)){
+  pkg_df$lib_size <- vapply(
+    pkg_df$Library,
+    function(x){
+      sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+    },
+  )
+}
+
+# test_that("lib_summary() returns expected results", {
+#   res <- lib_summary()
+#   expect_s3_class(res, "data.frame")
+#   expect_equal(ncol(res), 2)
+#   expect_equal(names(res), c("Library", "n_packages"))
+#   expect_type(res$Library, "character")
+#   expect_type(res$n_packages, "integer")
+# })
+#
+# test_that("lib_summary() fails appropriately", {
+#   #doesn't have to match on the entire error message - can take out (TRUE/FALSE)
+#   expect_error(lib_summary("foo"), "sizes must be a logical value (TRUE/FALSE)", fixed = TRUE)
+# })
+#
+# test_that("sizes argument works", {
+#   res <- lib_summary(sizes = TRUE)
+#   expect_equal(ncol(res), 3)
+#   expect_type(res$lib_size, "double")
+#})
